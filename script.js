3b936fae02ae5d7770f32a0e34661fc251334041
JaoWiki <84352768+JaoWiki@users.noreply.github.com>
2021-10-15 20:33:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,413 @@
+let carta1 = {
+  nome: "Gastrite",
+  foto:
+  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOkc3VDm4fjWNpejDVemyXJtS4cP1Sn083fvejKy9sCkzcZkGZZx1KR8Ow0wS__zDrmK8&usqp=CAU",  atributos: {
+    Transmissão: 10,
+    Prevenção: 30,
+    Mortalidade: 25
+  }
+};
+let carta2 = {
+  nome: "Cancer",
+  foto:
+    "https://images.theconversation.com/files/171727/original/file-20170531-25658-jprees.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=926&fit=clip",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 50,
+    Mortalidade: 90
+  }
+};
+let carta3 = {
+  nome: "Aids",
+  foto: "https://www.drakeillafreitas.com.br/wp-content/uploads/2017/11/Desaf%C3%ADo-para-principiantes-1000x509.png",
+  atributos: {
+    Transmissão: 70,
+    Prevenção: 50,
+    Mortalidade: 60
+  }
+};
+let carta4 = {
+  nome: "Covid-19",
+  foto:
+    "https://img.pebmed.com.br/wp-content/uploads/2020/12/10132359/primeiro_caso_reinfeccao_covid19.jpg.webp",
+  atributos: {
+    Transmissão: 90,
+    Prevenção: 20,
+    Mortalidade: 10
+  }
+};
+let carta5 = {
+  nome: "Influenza (Gripe)",
+  foto:
+  "http://www.saude.df.gov.br/wp-conteudo/uploads/2018/10/gripe.png",
+    atributos: {
+    Transmissão: 90,
+    Prevenção: 80,
+    Mortalidade: 15
+  }
+};
+let carta6 = {
+  nome: "Tétano",
+  foto:
+  "https://d3043uog1ad1l6.cloudfront.net/uploads/2021/05/Imagem2-31.png",
+    atributos: {
+    Transmissão: 20,
+    Prevenção: 10,
+    Mortalidade: 30
+  }
+};
+
+let carta7 = {
+  nome: "Herpes",
+  foto:
+    "https://audioderma.com.br/wp-content/uploads/2019/08/herpes.png",
+  atributos: {
+    Transmissão: 60,
+    Prevenção: 40,
+    Mortalidade: 10
+  }
+};
+
+let carta8 = {
+  nome: "Cistite",
+  foto:
+    "https://drrafaellocali.com.br/wp-content/uploads/2017/11/05-LOCALI-BLOG-CESTITE.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 30,
+    Mortalidade: 20
+  }
+};
+
+let carta9 = {
+  nome: "Ùlcera",
+  foto:
+    "https://static.tuasaude.com/media/article/yz/le/sintomas-de-ulcera-no-estomago_16663_l.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 20,
+    Mortalidade: 40
+  }
+};
+
+let carta10 = {
+  nome: "Faringite",
+  foto:
+    "https://otorrinopaulista.com.br/wp-content/uploads/2021/04/faringite_inflamacao_otorrinopaulista-1280x720.png",
+  atributos: {
+    Transmissão: 30,
+    Prevenção: 20,
+    Mortalidade: 40
+  }
+};
+
+let carta11 = {
+  nome: "Lúpus",
+  foto:
+    "https://static.tuasaude.com/media/article/cc/dc/lupus_24216_l.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 70,
+    Mortalidade: 20
+  }
+};
+
+let carta12 = {
+  nome: "Insuficiência Renal",
+  foto: "https://www.hcfmb.unesp.br/wp-content/uploads/2018/06/Doen%C3%A7a-Renal-Cr%C3%B4nica.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 50,
+    Mortalidade: 30
+  }
+};
+
+let carta13 = {
+  nome: "Aterosclerose",
+  foto:
+    "https://s3.static.brasilescola.uol.com.br/img/2019/10/aterosclerose-e-problemas-cardiacos.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 60,
+    Mortalidade: 80
+  }
+};
+
+let carta14 = {
+  nome: "Hipotireodismo",
+  foto:
+    "https://www.marciamariano.com.br/images/hipotireodismo.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 60,
+    Mortalidade: 20
+  }
+};
+
+let carta15 = {
+  nome: "Hipertireoidismo",
+  foto: "https://clinicaafetus.com.br/wp-content/uploads/hipertireodite.jpg",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 60,
+    Mortalidade: 20
+  }
+};
+
+let carta16 = {
+  nome: "Tuberculose",
+  foto: "https://clinicadopulmao.med.br/wp-content/uploads/2018/08/Tuberculose-%E2%80%93-Conheca-os-diferentes-tipos-Clinica-do-Pulmao.jpg",
+  atributos: {
+    Transmissão: 70,
+    Prevenção: 60,
+    Mortalidade: 80
+  }
+};
+
+let carta17 = {
+  nome: "Pneumonia",
+  foto: "https://www.news-medical.net/image.axd?picture=2021%2F5%2Fshutterstock_1160251117.jpg",
+  atributos: {
+    Transmissão: 70,
+    Prevenção: 50,
+    Mortalidade: 80
+  }
+};
+
+let carta18 = {
+  nome: "Doença Arterial Coronariana",
+  foto: "https://f1.media.brightcove.com/8/3850378299001/3850378299001_4817406789001_012-MyocardialInfarction.jpg?pubId=3850378299001&videoId=4378049368001",
+  atributos: {
+    Transmissão: 10,
+    Prevenção: 50,
+    Mortalidade: 60
+  }
+};
+
+let carta19 = {
+  nome: "Dengue",
+  foto: "https://s5.static.brasilescola.uol.com.br/be/conteudo/images/aedes-aegypti.jpg",
+  atributos: {
+    Transmissão: 80,
+    Prevenção: 30,
+    Mortalidade: 50
+  }
+};
+
+let carta20 = {
+  nome: "Ebola",
+  foto: "https://exame.com/wp-content/uploads/2020/06/GettyImages-685022417-e1591185239318.jpg?quality=70&strip=info",
+  atributos: {
+    Transmissão: 60,
+    Prevenção: 50,
+    Mortalidade: 70
+  }
+};
+
+let baralho = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10,
+  carta11,
+  carta12,
+  carta13,
+  carta14,
+  carta15,
+  carta16,
+  carta17,
+  carta18,
+  carta19,
+  carta20
+];
+let meuBaralho = [];
+
+function distribuiCartas() {
+  for (let minhasCartas = 0; minhasCartas < 10; minhasCartas++) {
+    let indiceAleatorio = Math.floor(Math.random() * baralho.length);
+
+    if (
+      indiceAleatorio >= baralho.length &&
+      indiceAleatorio >= baralho.length
+    ) {
+      indiceAleatorio = baralho.length - 1;
+    }
+    let flipCardAudio = document.querySelector("#flipCard");
+    let winAudio = document.querySelector("#win");
+    let loseAudio = document.querySelector("#lose");
+
+    meuBaralho.push(baralho.splice(indiceAleatorio, 1)[0]);
+  }
+}
+
+distribuiCartas();
+
+let cartaMaquina;
+let cartaJogador;
+let minhaCarta = document.querySelector("#cartaEscolhida");
+let campoJogador = document.querySelector(".cartaJogador");
+let campoMaquina = document.querySelector(".cartaMaquina");
+let pontos = document.querySelector(".pontuacao");
+let versus = document.querySelector(".versus");
+
+pontos.innerHTML = `Você possui ${meuBaralho.length} cartas | A máquina possui ${baralho.length} cartas`;
+
+function sortearCarta() {
+  numeroCartaMaquina = Math.floor(Math.random() * baralho.length);
+  numeroCartaJogador = Math.floor(Math.random() * meuBaralho.length);
+
+  if (
+    numeroCartaMaquina >= baralho.length &&
+    numeroCartaJogador >= meuBaralho.length
+  ) {
+    numeroCartaMaquina = baralho.length - 1;
+  }
+  cartaMaquina = baralho[numeroCartaMaquina];
+
+  cartaJogador = meuBaralho[numeroCartaJogador];
+  document.getElementById("campoBatalha").style.display = "none";
+  document.getElementById("btnSortear").style.display = "none";
+  document.getElementById("btnJogar").disabled = false;
+  document.getElementById("escolhaAtributo").style.display = "block";
+
+  minhaCarta.style.display = "block";
+
+  minhaCarta.innerHTML = `
+          <div class='carta'>
+          <div class="nomeCarta">${cartaJogador.nome}</div>
+          <img class="imgCarta" src="${cartaJogador.foto}" alt="">             
+          <div class="atributosCarta">
+          <div class="bg1"><span>Transmissão</span><span>${cartaJogador.atributos.Transmissão}</span></div>
+          <div class="bg2"><span>Prevenção</span><span>${cartaJogador.atributos.Prevenção}</span></div>
+          <div class="bg1"><span>Mortalidade</span><span>${cartaJogador.atributos.Mortalidade}</span></div>
+          </div></div>`;
+  exibirOpcoes();
+}
+
+function exibirOpcoes() {
+  let opcoes = document.getElementById("opcoes");
+  let opcoesTexto = "";
+  for (let atributo in cartaJogador.atributos) {
+    opcoesTexto += `<div><input  type='radio' name='atributo' value='${atributo}'>${atributo}</div>`;
+  }
+  opcoes.innerHTML = opcoesTexto;
+}
+
+function obtemAtributoSelecionado() {
+  let radioAtributo = document.getElementsByName("atributo");
+  for (let i = 0; i < radioAtributo.length; i++) {
+    if (radioAtributo[i].checked) {
+      return radioAtributo[i].value;
+    }
+  }
+}
+
+let resultado = document.querySelector("#resultado");
+
+function jogar() {
+  let atributoSelecionado = obtemAtributoSelecionado();
+  minhaCarta.innerHTML = "";
+  campoJogador.innerHTML = `
+      <div class='carta'>
+      <div class="nomeCarta">${cartaJogador.nome}</div>
+      <img class="imgCarta" src="${cartaJogador.foto}" alt="">             
+      <div class="atributosCarta">
+      <div class="bg1"><span>Transmissão</span><span>${cartaJogador.atributos.Transmissão}</span></div>
+      <div class="bg2"><span>Prevenção</span><span>${cartaJogador.atributos.Prevenção}</span></div>
+      <div class="bg1"><span>Mortalidade</span><span>${cartaJogador.atributos.Mortalidade}</span></div>
+      </div></div>`;
+
+  versus.innerHTML = `<img class="img-x" src="https://i.imgur.com/zRf2J4T.png" alt="">`;
+
+  campoMaquina.innerHTML = `
+      <div class='carta'>
+      <div class="nomeCarta">${cartaMaquina.nome}</div>
+      <img class="imgCarta" src="${cartaMaquina.foto}" alt="">             
+      <div class="atributosCarta">
+      <div class="bg1"><span>Transmissão</span><span>${cartaMaquina.atributos.Transmissão}</span></div>
+      <div class="bg2"><span>Prevenção</span><span>${cartaMaquina.atributos.Prevenção}</span></div>
+      <div class="bg1"><span>Mortalidade</span><span>${cartaMaquina.atributos.Mortalidade}</span></div>
+      </div></div>`;
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    resultado.innerHTML = "Você ganhou!";
+    meuBaralho.push;
+    meuBaralho.push(baralho.splice(numeroCartaMaquina, 1)[0]);
+    winAudio.play();
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    resultado.innerHTML = "Você perdeu!";
+    baralho.push(meuBaralho.splice(numeroCartaJogador, 1)[0]);
+    loseAudio.play();
+  } else {
+    resultado.innerHTML = "Empate";
+    flipCardAudio.play();
+  }
+
+  document.getElementById("campoBatalha").style.display = "flex";
+  document.getElementById("escolhaAtributo").style.display = "none";
+  document.getElementById("btnJogar").disabled = true;
+  document.getElementById("btnNovoJogo").disabled = false;
+  pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+}
+
+function novoJogo() {
+  if (baralho.length == 0) {
+    resultado.innerHTML = `As cartas do seu adversário acabaram, você ganhou o jogo!<br> Parabéns!`;
+    baralho = meuBaralho;
+    escodeButton();
+  } else if (meuBaralho.length == 0) {
+    resultado.innerHTML = `Suas cartas acabaram, você perdeu o jogo!<br> Jogue novamente!`;
+    escodeButton();
+  } else {
+    document.getElementById("btnNovoJogo").disabled = true;
+    minhaCarta.style.display = "none";
+    document.getElementById("escolhaAtributo").style.display = "none";
+    campoJogador.innerHTML = "";
+    versus.innerHTML = "";
+    campoMaquina.innerHTML = "";
+    resultado.innerHTML = "";
+    sortearCarta();
+    pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+  }
+}
+
+function reiniciarJogo() {
+  winAudio.load();
+  loseAudio.load();
+  meuBaralho = [];
+  distribuiCartas();
+  pontos.innerHTML = `Jogador possui ${meuBaralho.length} cartas | Máquina possui ${baralho.length} cartas`;
+  resultado.innerHTML = "";
+  document.getElementById("btnNovoJogo").disabled = true;
+  mostraButton();
+}
+
+function escodeButton() {
+  document.getElementById("reiniciarJogo").style.display = "inline-block";
+  document.getElementById("btnJogar").style.display = "none";
+  document.getElementById("btnNovoJogo").style.display = "none";
+  campoJogador.innerHTML = "";
+  versus.innerHTML = "";
+  campoMaquina.innerHTML = "";
+}
+
+function mostraButton() {
+  document.getElementById("reiniciarJogo").style.display = "none";
+  document.getElementById("btnJogar").style.display = "inline-block";
+  document.getElementById("btnNovoJogo").style.display = "inline-block";
+  document.getElementById("btnSortear").disabled = false;
+  document.getElementById("btnSortear").style.display = "inline-block";
+  document.getElementById("reiniciarJogo").style.display = "none";
+}
